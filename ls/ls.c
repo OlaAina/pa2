09cb7833c7fcb240172e0dc8c40cd7950b46ae8d
aoa90 <aoa90@scarletmail.rutgers.edu>
2021-10-09 16:38:10 -0400

    ls -l done?

--- ls/ls.c
+++ ls/ls.c
@@ -4,22 +4,45 @@
 #include<string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <grp.h>
+#include <pwd.h>
+#include <time.h>
 
 char **sortArray(char **sortThis,int n);
+void longlsprint(char** items);
 
 int main(int argc, char const *argv[]) {
+
+int mode = 0; 
+if(argc == 1){
+    mode = 1;
+}
+
+else if((argc == 2) && (strcmp(argv[1], "-l") == 0)){
+    mode = 2;
+}
+
+else{
+    printf("error\n");
+    return EXIT_SUCCESS;
+}
+
 DIR *d;
 struct dirent *dir;
 int n=0;
 int i=0;
 d = opendir("./");
+
 while((dir = readdir(d)) != NULL) {
     if ( !strcmp(dir->d_name, ".") || !strcmp(dir->d_name, "..") ){
 
     } 
     else {
         n++;
-        }
+    }
 }
 rewinddir(d);
 
@@ -37,17 +60,28 @@ while((dir = readdir(d)) != NULL) {
 }
 rewinddir(d);
 
-sortArray(items,i);
+sortArray(items, i);
 
-for(i=0; i<n; i++){
-printf("%s\n", items[i]);
+if(mode == 2) {
+    for(i=0; i<n; i++){
+        longlsprint(&items[i]);
+    }
 }
+
+else if (mode == 1){
+    for(i=0; i<n; i++){
+        printf("%s\n", items[i]);
+    }
+}
+
+
 for(int i=0;i<n;i++){
-        free(items[i]);
+    free(items[i]);
 }
 free(items);
 closedir(d);
 return 0;
+
 }
 
 char **sortArray(char **sortThis,int n){
@@ -67,4 +101,60 @@ char **sortArray(char **sortThis,int n){
     free(store);
     return sortThis;
 }
-           
+
+void longlsprint(char** items) {
+    struct stat fileStat;
+    stat(*items, &fileStat);
+    
+    //prints permissions
+    printf((S_ISDIR(fileStat.st_mode)) ? "d" : "-");
+    printf((fileStat.st_mode & S_IRUSR) ? "r" : "-");
+    printf((fileStat.st_mode & S_IWUSR) ? "w" : "-");
+    printf((fileStat.st_mode & S_IXUSR) ? "x" : "-");
+    printf((fileStat.st_mode & S_IRGRP) ? "r" : "-");
+    printf((fileStat.st_mode & S_IWGRP) ? "w" : "-");
+    printf((fileStat.st_mode & S_IXGRP) ? "x" : "-");
+    printf((fileStat.st_mode & S_IROTH) ? "r" : "-");
+    printf((fileStat.st_mode & S_IWOTH) ? "w" : "-");
+    printf((fileStat.st_mode & S_IXOTH) ? "x" : "-");
+
+
+    //get user info
+    struct passwd *p = getpwuid(fileStat.st_gid);
+    uid_t  uid=0;
+    if ((p = getpwuid(uid)) == NULL) {
+        printf("error"); 
+        return;
+    }
+
+    if(p->pw_name == NULL) {
+        printf(" %d", (int)p);
+    }
+    else {
+        printf(" %s", p->pw_name);
+    }
+ 
+    //get group info
+    struct group *grp = getgrgid(fileStat.st_gid);
+    if(grp->gr_name == NULL){
+        printf(" %d", (int)grp);
+    }
+    else{
+        printf(" %s", grp->gr_name);
+    }
+
+    //print size 
+    if (stat(*items, &fileStat) == 0) { 
+        printf(" %lld", fileStat.st_size);
+    }
+
+    //date 
+    char time[50];
+    strftime(time, 50, "%b %d %H:%M", localtime(&fileStat.st_mtime));
+    printf (" %s", time);
+
+    printf(" %s", *items);
+    printf("\n");
+    return;
+}
+
